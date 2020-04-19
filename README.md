f6dbc9edf5cb87fcf7e43982def52abbe29bfe8e
Edward Reyes <reyes.edwardf@gmail.com>
2020-04-18 20:35:48 -0700

    readme img

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # hplussport
+
+From this tutorial, I worked with the DOM to manipulate form fields, read data,
+checking input for errors, and displaying the results of shipping calculations.
+
+![Submit Form Disabled](assets/screenshot-1.png)
--- script.js
+++ script.js
@@ -2,19 +2,26 @@
 (function() {
   "use strict";
 
-  document
-    .getElementById("cart-hplus")
-    .addEventListener("submit", estimateTotal);
+  document.addEventListener('DOMContentLoaded' , function() { // when page loaded sufficiently that the DOM is ready
+    document
+      .getElementById("cart-hplus")
+      .addEventListener("submit", estimateTotal)
+
+      var btnEstimate = document.getElementById('btn-estimate'); // Get button reference
+      btnEstimate.disabled = true;
+  }); 
 
   function estimateTotal(event) {
+    // Prevent the page from reloading
     event.preventDefault();
 
     var state = document.getElementById('s-state');
 
-    // Check its value if its empty
+    // Check its value if no item was selected
     if (state.value === "") {
       alert("Please choose your shipping state");
 
+      // add keyboard focus to appear on this item.
       state.focus();
 
       return;
