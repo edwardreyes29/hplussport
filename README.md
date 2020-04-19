b2380c5f717bc3c7d6d326d17980865d64536480
Edward Reyes <reyes.edwardf@gmail.com>
2020-04-18 20:47:52 -0700

    disable Estimate button

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # hplussport
 
-From this tutorial, I worked with the DOM to manipulate form fields, read data,
+In this project, I used JavaScript to work with the DOM to manipulate form fields, read data,
 checking input for errors, and displaying the results of shipping calculations.
 
+# Submit button will be disabled if the state selection is has no value
 ![Submit Form Disabled](assets/screenshot-1.png)
+![Submit Form Disabled](assets/screenshot-2.png)
--- script.js
+++ script.js
@@ -3,28 +3,42 @@
   "use strict";
 
   document.addEventListener('DOMContentLoaded' , function() { // when page loaded sufficiently that the DOM is ready
+
+    var state = document.getElementById('s-state');
+
     document
       .getElementById("cart-hplus")
       .addEventListener("submit", estimateTotal)
 
       var btnEstimate = document.getElementById('btn-estimate'); // Get button reference
+
       btnEstimate.disabled = true;
+
+      // Check when value of state changes
+      state.addEventListener('change', function() {
+        if (state.value === "") {
+          btnEstimate.disabled = true;
+        } else { // if state value changes
+          btnEstimate.disabled = false;
+        }
+      });
+
+      function estimateTotal(event) {
+        // Prevent the page from reloading
+        event.preventDefault();
+    
+        // Check its value if no item was selected
+        if (state.value === "") {
+          alert("Please choose your shipping state");
+    
+          // add keyboard focus to appear on this item.
+          state.focus();
+    
+          return;
+        } 
+      }
+
   }); 
 
-  function estimateTotal(event) {
-    // Prevent the page from reloading
-    event.preventDefault();
-
-    var state = document.getElementById('s-state');
-
-    // Check its value if no item was selected
-    if (state.value === "") {
-      alert("Please choose your shipping state");
-
-      // add keyboard focus to appear on this item.
-      state.focus();
-
-      return;
-    } 
-  }
+  
 })();
