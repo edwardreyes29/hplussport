d3db872f74a9dfc89174cc306617f36af80a8e80
Edward Reyes <reyes.edwardf@gmail.com>
2020-04-20 15:18:38 -0700

    estimated cost input

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ checking input for errors, and displaying the results of shipping calculations.
 Submit button will be disabled if the state selection is has no value
 ![Submit Form Disabled](assets/screenshot-1.png)
 ![Submit Form Disabled](assets/screenshot-2.png)
+Estimate Total of total quantity of items, state tax, and shipping methods
+![Submit Form Disabled](assets/screenshot-3.png)
--- script.js
+++ script.js
@@ -36,9 +36,45 @@
     
           return;
         } 
+
+
+        // Collect quantities of all items in the cart.
+        var itemBball = parseInt(document.getElementById("txt-q-bball").value, 10) || 0, // if a number or set to zero if empty
+          itemJersey = parseInt(document.getElementById("txt-q-jersey").value, 10) || 0,
+          itemPower = parseInt(document.getElementById("txt-q-power").value, 10) || 0,
+          shippingState = state.value,
+          shippingMethod = document.querySelector("[name=r_method]:checked")
+            .value || "";
+
+        var totalQty = itemBball + itemJersey + itemPower,
+          shippingCostPer,
+          shippingCost,
+          taxFactor = 1,
+          estimate,
+          totalItemPrice = 90 * itemBball + 25 * itemJersey + 30 * itemPower;
+
+        if (shippingState === "CA") {
+          taxFactor = 1.075;
+        }
+
+        switch (shippingMethod) {
+          case "usps":
+            shippingCostPer = 2;
+            break;
+          case "ups":
+            shippingCostPer = 3;
+            break;
+          default:
+            shippingCostPer = 0;
+            break;
+        }
+
+        shippingCost = shippingCostPer * totalQty;
+
+        estimate = "$" + (totalItemPrice * taxFactor + shippingCost).toFixed(2); // Shipping cost not subject to the tax
+
+        document.getElementById("txt-estimate").value = estimate;
+
       }
-
-  }); 
-
-  
+  });   
 })();
